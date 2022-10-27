e61ac512954ea35fbbd2bc018d8352a14291be70
Glenn <fallwich@naver.com>
2022-10-28 02:52:07 +0900

    add if

--- human_body.py
+++ human_body.py
@@ -1,12 +1,11 @@
+import time
 people = 16
 led = 19
 GPIO.setup(led, GPIO.OUT)
 GPIO.setup(people, GPIO.IN)
-try:
-    while True:
-        GPIO.output(led, GPIO.LOW)       
-        elif GPIO.input(people) ==1:
-            GPIO.output(led, GPIO.HIGH)  #인체 감지 시 led on 후에 침입자 사진 찍는 곳에 활용
-        time.sleep(2)
-except KeyboardInterrupt:
-    pass
+while True:
+    if GPIO.input(people) ==0:
+        GPIO.output(led, GPIO.LOW)
+    elif GPIO.input(people) ==1:
+        GPIO.output(led, GPIO.HIGH)  #인체 감지 시 led on 후에 침입자 사진 찍는 곳에 활용
+    time.sleep(2)
